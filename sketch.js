711dccc6d92fd1699bf952a9f26250937f440728
mukundgupta <70640082+mukundgupta@users.noreply.github.com>
2020-10-31 18:34:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+//Create variables here
+var dog;
+var happyDog;
+var database;
+var foodStock;
+var foodS;
+
+function preload()
+{
+  //load images here
+  dogimg = loadImage("images/dogImg.png")
+  dog2img = loadImage("images/dogImg1.png")
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  dog = createSprite(250,250,50,50)
+  dog.addImage("img",dogimg)
+  dog.scale = 0.2
+  foodStock = database.ref('Food')
+  foodStock.on("value",readStock)
+}
+
+
+function draw() {  
+background(46, 139, 87)
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS)
+  dog.addImage("img",dog2img)
+}
+  drawSprites();
+  //add styles here
+  textSize(30)
+  fill("red")
+text("Food Remaining: " + foodS,120,400)
+}
+
+function writeStock(x){
+  if(x<=0){
+    x = 0;
+  }else{
+    x = x-1;
+  }
+database.ref('/').update({
+  Food:x
+})
+}
+
+function readStock(data){
+foodS = data.val();
+}
